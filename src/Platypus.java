beff185f5e4f1211d6d87ac322f8f57809d76714
philliphjhuang <30792325+philliphjhuang@users.noreply.github.com>
2019-01-01 18:22:02 -0800

    platypus,Pig+Candy

---
+++ src/Platypus.java
@@ -0,0 +1,9 @@
+public class Platypus {
+      private String name;
+Platypus(String name){
+	this.name = name;
+}
+      	void sayHi(){
+            	System.out.println("The platypus " + name + " is smarter than your average platypus.");
+      	}
+}
---
+++ src/PlatypusRunner.java
@@ -0,0 +1,7 @@
+public class PlatypusRunner {
+
+public static void main(String[] args) {
+	Platypus bob = new Platypus("bob");
+	bob.sayHi();
+}
+}
